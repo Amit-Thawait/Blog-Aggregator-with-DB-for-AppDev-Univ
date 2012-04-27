8cc7e99ca0cda120f4fc9010feeb11281f52f590
Amit Thawait <amit17thawait@gmail.com>
2012-04-27 21:28:37 +0530

    new method for rss parsing added

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -29,38 +29,114 @@ class Blog < ActiveRecord::Base
       end
   end
   
+  # #  Generic Method to read all blogs post
+  # def read_blog_posts(blog)
+    # doc = Nokogiri::HTML(open("#{blog.blog_url}"))      
+    # xml_url = doc.css('head link[rel="alternate"]').first['href']
+    # xml_type = doc.css('head link[rel="alternate"]').first['type']
+    # xml_doc_url= xml_url.include?("http:") ? xml_url : "http:" + xml_url
+    # @xml_doc = Nokogiri::XML(open("#{xml_doc_url}"))      
+    # if !@xml_doc.nil?
+        # @xml_doc.css('entry').each do |node|
+          # blog_title = @xml_doc.css('title').first.text        
+          # blog.blog_title = blog_title
+          # blog_author = @xml_doc.css('author name').first.nil?  ? '' : @xml_doc.css('author name').first.text #@xml_doc.css('author name').first.text
+          # blog.blog_author =  blog_author.blank? ? blog_title : blog_author
+          # blog.save!
+          # @post = Post.new
+          # title = node.css('title').text 
+          # @post.title = title
+          # @post.blog_id = blog.id          
+          # @post.url = "#"
+          # if !title.blank?         
+            # post_url = node.css('link[rel="alternate"]') #node.css('link').text
+            # @post.url = !post_url.blank? ? post_url[0]['href'] : node.css('link')[0]['href']
+          # end    
+          # @post.content = node.css('content').text#node.content
+          # @post.author = blog_title
+          # post_author = node.css('author name').text         
+          # @post.author = !post_author.blank? ? post_author : blog_title                                 
+          # published_date = node.css('updated').text 
+          # @post.post_date = Time.parse(published_date.gsub('T'," "))          
+          # @post.save!
+        # end         
+      # end    
+  # end
+
+
   #  Generic Method to read all blogs post
+  #http://nokogiri.org/Nokogiri/XML/Node.html#method-i-xpath
   def read_blog_posts(blog)
     doc = Nokogiri::HTML(open("#{blog.blog_url}"))      
     xml_url = doc.css('head link[rel="alternate"]').first['href']
+    xml_type = doc.css('head link[rel="alternate"]').first['type']
     xml_doc_url= xml_url.include?("http:") ? xml_url : "http:" + xml_url
     @xml_doc = Nokogiri::XML(open("#{xml_doc_url}"))      
     if !@xml_doc.nil?
-        @xml_doc.css('entry').each do |node|
-          blog_title = @xml_doc.css('title').first.text        
-          blog.blog_title = blog_title
-          blog_author = @xml_doc.css('author name').first.text
-          blog.blog_author =  blog_author.blank? ? blog_title : blog_author
-          blog.save!
-          @post = Post.new
-          title = node.css('title').text 
-          @post.title = title
-          @post.blog_id = blog.id          
-          @post.url = "#"
-          if !title.blank?         
-            post_url = node.css('link[rel="alternate"]')
-            @post.url = !post_url.blank? ? post_url[0]['href'] : node.css('link')[0]['href']
-          end    
-          @post.content = node.css('content').text
-          @post.author = blog_title
-          post_author = node.css('author name').text         
-          @post.author = !post_author.blank? ? post_author : blog_title                                 
-          published_date = node.css('updated').text 
-          @post.post_date = Time.parse(published_date.gsub('T'," "))          
-          @post.save!
-        end         
-      end    
+      if xml_type == 'application/atom+xml'
+        atom_parsing(blog,@xml_doc)
+      end
+
+      if xml_type == 'application/rss+xml'
+        rss_parsing(blog,@xml_doc)
+      end       
+    end    
   end
+
+
+  def atom_parsing(blog,xml_doc)
+    xml_doc.css('entry').each do |node|
+      blog_title = xml_doc.css('title').first.text        
+      blog.blog_title = blog_title
+      blog_author = xml_doc.css('author name').first.nil?  ? '' : xml_doc.css('author name').first.text #@xml_doc.css('author name').first.text
+      blog.blog_author =  blog_author.blank? ? blog_title : blog_author
+      blog.save!
+      post = Post.new
+      title = node.css('title').text 
+      post.title = title
+      post.blog_id = blog.id          
+      post.url = "#"
+      if !title.blank?         
+        post_url = node.css('link[rel="alternate"]') #node.css('link').text
+        post.url = !post_url.blank? ? post_url[0]['href'] : node.css('link')[0]['href']
+      end    
+      post.content = node.css('content').text#node.content
+      post.author = blog_title
+      post_author = node.css('author name').text         
+      post.author = !post_author.blank? ? post_author : blog_title                                 
+      published_date = node.css('updated').text 
+      post.post_date = Time.parse(published_date.gsub('T'," "))          
+      post.save!
+    end      
+  end 
+  
+  
+  def rss_parsing(blog,xml_doc)
+    xml_doc.css('item').each do |node|
+       blog_title = xml_doc.css('title').first.text
+       blog.blog_title = blog_title
+       blog_author = xml_doc.css('author name').first.nil?  ? '' : xml_doc.css('author name').first.text
+       blog.blog_author =  blog_author.blank? ? blog_title : blog_author
+       blog.save!
+       post = Post.new
+       title = node.css('title').text 
+       post.title = title
+       post.blog_id = blog.id
+       post.url = "#"
+       if !title.blank?
+          post.url = node.css('link').text
+       end
+       post.content = node.content
+       post.author = blog_title
+       post_author = node.css('author name').text
+       post.author = !post_author.blank? ? post_author : blog_title 
+       published_date = node.css('pubDate').text
+       post.post_date = published_date.to_date
+       post.save!
+    end    
+  end
+    
+
   
   #This method is used to read posts from pat's blogs
   #We know that separate method for each blog is not a good idea. It's quite difficult to write a generic method since everyone is following different styles for their blogs.
